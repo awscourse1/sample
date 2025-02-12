38d4a8027a38095ffb0b208843b5b7f3cc0c414a
flo <florian.berhault07@gmail.com>
2025-02-12 16:15:28 +0100

    variabilisé sample.tf et création tfvars

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,25 +1,23 @@
 provider "google" {
-  project     = "votre-projet-gcp"
-  region      = "us-central1"
-  credentials = file("chemin-vers-votre-fichier-de-cle.json")
+  project     = var.project_id
+  region      = var.region
+  credentials = file(var.credentials_file)
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "mon-instance-gcp"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.image
     }
   }
 
   network_interface {
     network = "default"
-    access_config {
-      // Permet une IP publique
-    }
+    access_config {}
   }
 
   metadata_startup_script = <<EOT
@@ -33,5 +31,6 @@ resource "google_compute_instance" "vm_instance" {
 }
 
 output "instance_ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+  description = "Adresse IP publique de l'instance"
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
